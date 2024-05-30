730a3baeba0abbdb5b697a63f2c4f73fc9655b0d
Uriel Zazueta <uriel.zazueta.barreras@gmail.com>
2024-05-29 21:37:27 -0700

    Test example

---
+++ FW/tests/Example.cpp
@@ -0,0 +1,21 @@
+#include "CppUTest/TestHarness.h"
+
+extern "C"
+{
+
+}
+
+TEST_GROUP(Example)
+{
+   void setup()
+   {  
+   }
+   void teardown()
+   {
+   }
+};
+
+TEST(Example, T01)
+{
+   //FAIL("Fail test example");
+}
